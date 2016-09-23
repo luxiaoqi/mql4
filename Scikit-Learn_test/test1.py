f6e86aa6856aff70949ae471e2dd08045a3e195a
luxiaoqi <luxiaoqi1987@163.com>
2016-09-23 16:32:42 +0800

    lxq

---
+++ Scikit-Learn_test/test1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import urllib
+from sklearn import metrics
+from sklearn.ensemble import ExtraTreesClassifier
+import scipy
+# url with dataset
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+# download the file
+raw_data = urllib.urlopen(url)
+# load the CSV file as a numpy matrix
+dataset = np.loadtxt(raw_data, delimiter=",")
+# separate the data from the target attributes
+X = dataset[:,0:7]
+y = dataset[:,8]
+#print X
+
+
+
+
+model = ExtraTreesClassifier()
+model.fit(X, y)
+# display the relative importance of each attribute
+print(model.feature_importances_)
